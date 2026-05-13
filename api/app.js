cede94870dbb55b43442d32112da06d7774f22b6
cverhoog-tech <c.verhoog@gmail.com>
2026-05-14 01:51:17 +0200

    Add files via upload

---
+++ api/app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async function handler(req, res) {
+  try {
+    const htmlPath = path.join(process.cwd(), 'index.html');
+    const html = fs.readFileSync(htmlPath, 'utf-8');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
+    res.status(200).send(html);
+  } catch (error) {
+    res.status(500).send('FamilyApp loader error: ' + (error && error.message ? error.message : String(error)));
+  }
+};
