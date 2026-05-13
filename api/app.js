6a36f7fabbc276ea6898c95e551cf11c3d3cf495
cverhoog-tech <c.verhoog@gmail.com>
2026-05-14 00:41:32 +0200

    Inject controlled session shell

--- api/app.js
+++ api/app.js
@@ -15,8 +15,10 @@ module.exports = async function handler(req, res) {
 <link rel="stylesheet" href="/trade-engine.css">
 <script src="/trade-engine.js" defer></script>
 <link rel="stylesheet" href="/src/app/homePremiumPolish.css?v=5">
+<link rel="stylesheet" href="/src/app/sessionShell.css?v=1">
 <script src="/src/app/disableLegacyQuestRouteHotfix.js" defer></script>
 <script src="/src/app/homePremiumPolish.js?v=5" defer></script>
+<script src="/src/app/sessionShell.js?v=1" defer></script>
 
 <script type="module" id="modern-bridge-loader">
   import { mountLegacyFeedBridge } from '/src/app/legacy-feed-bridge.js';
