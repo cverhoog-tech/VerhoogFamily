ec93d904f19d33fe0b52a67ad4a126b647775717
cverhoog-tech <c.verhoog@gmail.com>
2026-05-14 00:04:31 +0200

    Inject targeted task submit guard

--- api/app.js
+++ api/app.js
@@ -16,6 +16,7 @@ module.exports = async function handler(req, res) {
 <script src="/trade-engine.js" defer></script>
 <link rel="stylesheet" href="/src/app/homePremiumPolish.css?v=5">
 <script src="/src/app/homePremiumPolish.js?v=5" defer></script>
+<script src="/src/app/taskCreateSubmitGuard.js?v=1" defer></script>
 
 <script type="module" id="modern-bridge-loader">
   import { mountLegacyFeedBridge } from '/src/app/legacy-feed-bridge.js';
