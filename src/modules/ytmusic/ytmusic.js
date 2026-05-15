4cb6b8397f284bfdd9b0dc46095446a0d2a3dde5
cverhoog-tech <c.verhoog@gmail.com>
2026-05-15 13:03:34 +0200

    fix(tasks): load navigation stability layer

--- src/modules/ytmusic/ytmusic.js
+++ src/modules/ytmusic/ytmusic.js
@@ -84,5 +84,6 @@ function closeYtBar(){
   loadScriptOnce('group-quest-premium-js','src/modules/tasks/groupQuestPremium.js');
   loadScriptOnce('quest-overview-integration-js','src/modules/tasks/questOverviewIntegration.js');
   loadScriptOnce('quest-overview-watcher-js','src/modules/tasks/questOverviewWatcher.js');
+  loadScriptOnce('task-navigation-stability-js','src/modules/tasks/taskNavigationStability.js');
 })();
 
