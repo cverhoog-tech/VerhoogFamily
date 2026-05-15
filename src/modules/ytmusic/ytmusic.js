c60fea0972c7096a71976f9c542721973cc517ac
cverhoog-tech <c.verhoog@gmail.com>
2026-05-15 14:00:18 +0200

    style(tasks): load raid card polish module

--- src/modules/ytmusic/ytmusic.js
+++ src/modules/ytmusic/ytmusic.js
@@ -83,6 +83,7 @@ function closeYtBar(){
   loadScriptOnce('quest-adapter-js','src/core/questAdapter.js');
   loadScriptOnce('group-quest-premium-js','src/modules/tasks/groupQuestPremium.js');
   loadScriptOnce('group-quest-layout-fix-js','src/modules/tasks/groupQuestLayoutFix.js');
+  loadScriptOnce('raid-card-polish-js','src/modules/tasks/raidCardPolish.js');
   loadScriptOnce('quest-overview-integration-js','src/modules/tasks/questOverviewIntegration.js');
   loadScriptOnce('quest-overview-watcher-js','src/modules/tasks/questOverviewWatcher.js');
   loadScriptOnce('task-navigation-stability-js','src/modules/tasks/taskNavigationStability.js');
