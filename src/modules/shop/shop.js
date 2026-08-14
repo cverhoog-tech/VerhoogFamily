47a510402b3fab134e2b38975bf0c18a38ccb65a
cverhoog-tech <c.verhoog@gmail.com>
2026-08-14 16:09:05 +0200

    Refresh shopping receipt finance bridge version

--- src/modules/shop/shop.js
+++ src/modules/shop/shop.js
@@ -33,7 +33,7 @@
       .then(function(){ return loadScriptOnce('modal-manager-js', 'src/core/modalManager.js', function(){ return !!window.ModalManager; }); })
       .then(function(){ return loadScriptOnce('bottom-sheet-js', 'src/core/bottomSheet.js', function(){ return !!window.BottomSheet; }); })
       .then(function(){ return loadScriptOnce('grocery-quick-add-modal-js', 'src/core/groceryQuickAddModal.js', function(){ return !!window.GroceryQuickAddModal; }); })
-      .then(function(){ return loadScriptOnce('shopping-receipt-finance-js', 'src/modules/shop/shoppingReceiptFinance.js?v=2', function(){ return !!window.ShoppingReceiptFinance; }); })
+      .then(function(){ return loadScriptOnce('shopping-receipt-finance-js', 'src/modules/shop/shoppingReceiptFinance.js?v=3', function(){ return !!window.ShoppingReceiptFinance; }); })
       .then(function(){
         if(window.GroceryQuickAddModal && typeof window.GroceryQuickAddModal.installButton === 'function') window.GroceryQuickAddModal.installButton();
         if(window.ShoppingReceiptFinance && typeof window.ShoppingReceiptFinance.render === 'function') window.ShoppingReceiptFinance.render();
