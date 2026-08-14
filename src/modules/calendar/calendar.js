32b56712d42902d341c5f9968ceab971fd45dc6d
cverhoog-tech <c.verhoog@gmail.com>
2026-08-14 11:39:45 +0200

    Load automatic Google Calendar sync

--- src/modules/calendar/calendar.js
+++ src/modules/calendar/calendar.js
@@ -15,10 +15,10 @@
   }
 
   load('src/modules/calendar/calendarLegacy.js?v=1', function(){
-    load('src/modules/calendar/calendarSharedLive.js?v=1', function(){
+    load('src/modules/calendar/calendarSharedLive.js?v=2', function(){
       load('src/modules/calendar/calendarPremiumUi.js?v=2', function(){
         load('src/modules/calendar/calendarMealPlanIntegration.js?v=1', function(){
-          load('src/modules/calendar/calendarSimpleGoogle.js?v=1');
+          load('src/modules/calendar/calendarGoogleSync.js?v=1');
         });
       });
     });
