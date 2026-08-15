a77bd36bfeaa3b3b84f88cd694f0ac14d6d8123f
cverhoog-tech <c.verhoog@gmail.com>
2026-08-15 22:01:30 +0200

    chore: load canonical activity service and producers

--- api/app.js
+++ api/app.js
@@ -18,7 +18,8 @@ module.exports = async function handler(req, res) {
       '<script src="src/modules/feed/feedSharedData.js?v=4"></script>\n'
       + '  <script src="src/modules/feed/feed.js?v=6"></script>\n'
       + '  <script src="src/modules/feed/feedInteractionController.js?v=5"></script>\n'
-      + '  <script src="src/platform/activity/householdActivity.js?v=5"></script>\n'
+      + '  <script src="src/platform/activity/householdActivity.js?v=6"></script>\n'
+      + '  <script src="src/platform/activity/activityDomainProducers.js?v=1"></script>\n'
       + '  <script src="src/modules/feed/feedActivityPresentation.js?v=5"></script>'
     );
 
@@ -36,7 +37,7 @@ module.exports = async function handler(req, res) {
     html = html.replace('<script src="src/modules/meals/mealPlanStore.js"></script>','<script src="src/modules/meals/mealPlanStore.js?v=2"></script>');
     html = html.replace('<script src="src/modules/meals/mealPlannerBottomSheetBridge.js"></script>','<script src="src/modules/meals/mealPlannerBottomSheetBridge.js?v=1"></script>\n  <script src="src/modules/meals/mealPlannerContextGuard.js?v=1"></script>');
 
-    html = html.replace('</body>','<script src="src/modules/meals/mealPlannerContextGuard.js?v=1"></script>\n<script src="src/modules/tasks/taskOverviewCanonical.js?v=5"></script>\n<script src="src/app/uiConsistencyPolish.js?v=1"></script>\n<script src="src/core/mobileUxFixes.js?v=2"></script>\n<script src="src/app/freshStartReset.js?v=1"></script></body>');
+    html = html.replace('</body>','<script src="src/modules/tasks/taskOverviewCanonical.js?v=5"></script>\n<script src="src/app/uiConsistencyPolish.js?v=1"></script>\n<script src="src/core/mobileUxFixes.js?v=2"></script>\n<script src="src/app/freshStartReset.js?v=1"></script></body>');
 
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.setHeader('Cache-Control', 'no-store, max-age=0');
