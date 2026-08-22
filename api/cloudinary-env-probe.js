c873c21d081b93979495bac5382815fc043a96eb
cverhoog-tech <c.verhoog@gmail.com>
2026-08-22 12:02:26 +0200

    chore: remove Cloudinary environment probe

--- api/cloudinary-env-probe.js
+++
@@ -1,9 +0,0 @@
-module.exports=function handler(req,res){
-  if(req.method!=='GET'){res.status(405).json({ok:false});return;}
-  res.status(200).json({
-    ok:true,
-    cloudName:!!(process.env.CLOUDINARY_CLOUD_NAME||process.env.CLOUDINARY_URL),
-    apiKey:!!(process.env.CLOUDINARY_API_KEY||process.env.CLOUDINARY_URL),
-    apiSecret:!!(process.env.CLOUDINARY_API_SECRET||process.env.CLOUDINARY_URL)
-  });
-};
