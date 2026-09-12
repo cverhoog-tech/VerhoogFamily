92641ae57a699f7fb951b730b627a01bdf290ce6
cverhoog-tech <c.verhoog@gmail.com>
2026-09-12 23:35:22 +0200

    fix(tasks-v3): serve popup close control geometry hardening

--- api/app-v7.js
+++ api/app-v7.js
@@ -57,6 +57,9 @@ module.exports = async function handler(req, res) {
   if (!body.includes('tasksV3Polish.css?v=2')) {
     body = body.replace('</head>', '  <link rel="stylesheet" href="/src/styles/tasksV3Polish.css?v=2">\n</head>');
   }
+  if (!body.includes('tasksV3InteractionPolish.css?v=1')) {
+    body = body.replace('</head>', '  <link rel="stylesheet" href="/src/styles/tasksV3InteractionPolish.css?v=1">\n</head>');
+  }
   if (!body.includes('loginBrandV7.js?v=2')) {
     body = body.replace('</body>', '  <script src="/src/core/loginBrandV7.js?v=2"></script>\n</body>');
   }
