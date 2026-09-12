7473ae6d5551205510fad12d54e3f1f2a5c71941
cverhoog-tech <c.verhoog@gmail.com>
2026-09-12 23:26:34 +0200

    chore(tasks-v3): cache-bust checkbox performance fixes

--- api/app-v7.js
+++ api/app-v7.js
@@ -54,8 +54,8 @@ module.exports = async function handler(req, res) {
   if (!body.includes('tasksV3.css?v=2')) {
     body = body.replace('</head>', '  <link rel="stylesheet" href="/src/styles/tasksV3.css?v=2">\n</head>');
   }
-  if (!body.includes('tasksV3Polish.css?v=1')) {
-    body = body.replace('</head>', '  <link rel="stylesheet" href="/src/styles/tasksV3Polish.css?v=1">\n</head>');
+  if (!body.includes('tasksV3Polish.css?v=2')) {
+    body = body.replace('</head>', '  <link rel="stylesheet" href="/src/styles/tasksV3Polish.css?v=2">\n</head>');
   }
   if (!body.includes('loginBrandV7.js?v=2')) {
     body = body.replace('</body>', '  <script src="/src/core/loginBrandV7.js?v=2"></script>\n</body>');
@@ -67,7 +67,7 @@ module.exports = async function handler(req, res) {
     body = body.replace('</body>', '  <script src="/src/core/cleaningMoreMenuIcon.js?v=1"></script>\n</body>');
   }
   if (!body.includes('taskPresentationModelV3.js?v=3')) {
-    body = body.replace('</body>', '  <script src="/src/modules/tasks/v3/taskPresentationModelV3.js?v=3"></script>\n  <script src="/src/modules/tasks/v3/taskOverviewV3.js?v=2"></script>\n  <script src="/src/modules/tasks/v3/taskDetailV3.js?v=2"></script>\n</body>');
+    body = body.replace('</body>', '  <script src="/src/modules/tasks/v3/taskPresentationModelV3.js?v=3"></script>\n  <script src="/src/modules/tasks/v3/taskOverviewV3.js?v=3"></script>\n  <script src="/src/modules/tasks/v3/taskDetailV3.js?v=3"></script>\n</body>');
   }
 
   Object.keys(headers).forEach((name) => res.setHeader(name, headers[name]));
