2868170e6d6b31f2fe73c4046fb952fe1cbfc727
cverhoog-tech <c.verhoog@gmail.com>
2026-09-13 10:37:22 +0200

    Load Cleaning detail escape and avatar repair

--- src/modules/cleaning/cleaningCompanionLoader.js
+++ src/modules/cleaning/cleaningCompanionLoader.js
@@ -5,9 +5,10 @@ import './cleaningCollaborationExperience.js?v=2';
 import './cleaningHistoryV22.js?v=1';
 import './cleaningDetailVisualV221.js?v=1';
 import './cleaningDetailVisualV221Refinement.js?v=1';
+import './cleaningDetailEscapeAvatarFixV1.js?v=1';
 import './cleaningOccurrenceCommandsV23.js?v=2';
 import './cleaningOccurrenceControlsV23.js?v=3';
 import './cleaningAssignmentExperienceV25.js?v=2';
 
-window.CleaningV23Companions=Object.freeze({version:'2.5.1',cleaningOnly:true});
-export const CLEANING_COMPANION_LOADER_VERSION='2.5.1';
+window.CleaningV23Companions=Object.freeze({version:'2.5.2',cleaningOnly:true});
+export const CLEANING_COMPANION_LOADER_VERSION='2.5.2';
